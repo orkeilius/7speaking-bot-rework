4ca235a40d08a920708356807b18f8f68228b7d9
orkeilius <63045634+orkeilius@users.noreply.github.com>
2025-10-18 15:57:30 +0200

    add hide button on overlay

--- src/contents/overlay/index.tsx
+++ src/contents/overlay/index.tsx
@@ -1,6 +1,6 @@
 import type {PlasmoCSConfig, PlasmoGetStyle} from "plasmo";
 import styleText from "data-text:~style.css";
-import {FaRegCirclePlay, FaRegCirclePause} from "react-icons/fa6";
+import {FaRegCirclePlay, FaRegCirclePause, FaEyeSlash} from "react-icons/fa6";
 import {useEffect, useState} from "react";
 import {storageService} from "~contents/services/StorageService";
 
@@ -40,6 +40,11 @@ const Overlay = () => {
         await storageService.setActive(!active);
         setActive(!active);
     }
+    const toggleVisible = async () => {
+        await storageService.setShowOverlay(!visible);
+        await storageService.setActive(false)
+        setVisible(!visible);
+    }
 
     if (!visible) {
         return null;
@@ -47,11 +52,14 @@ const Overlay = () => {
 
     return (
         <div className="fixed bottom-0 left-0 flex z-[9999] m-2 px-4 py-2 text-2xl border bg-bg-2 rounded-full p-2 items-center shadow shadow-primary">
+
+            <FaEyeSlash className="text-text-2 mr-2 border-2 border-text-2 rounded-full" onClick={toggleVisible}/>
             {active ?
                 <FaRegCirclePause className="text-secondary mr-1" onClick={toggleActive}/>
              :
                 <FaRegCirclePlay className="text-primary mr-1" onClick={toggleActive}/>
             }
+
             <span className="overlay-text">{text}</span>
 
 
